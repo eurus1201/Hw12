bde9788db09ff58ece3428b5994695fd4b2b93f2
eurus1201 <khodabakhshian.s@gmail.com>
2019-08-06 19:13:18 +0430

    add lap to stopWAtch

--- src/_pages/stopWatch.page.js
+++ src/_pages/stopWatch.page.js
@@ -1,61 +1,68 @@
-import React,{Component} from 'react';
-import { StopWatch,ClockButton } from "../_components";
+import React, { Component } from 'react';
+import { StopWatch, ClockButton } from "../_components";
 import { withRouter } from "react-router-dom";
 class StopWatchPage extends Component {
 
     state = {
         counter: 0,
-        start: false
+        start: false,
+        laps: []
     }
 
     goBack = () => {
         this.props.history.push('/');
     }
 
-    startStopWatch = () =>{
-        this.setState({start: true})
-        this.interval = setInterval(()=>{
-            this.setState({counter: this.state.counter + 1})
-        },1000)
+    startStopWatch = () => {
+        this.setState({ start: true })
+        this.interval = setInterval(() => {
+            this.setState({ counter: this.state.counter + 1 })
+        }, 1000)
     }
 
-    addLap = () =>{
-
+    addLap = () => {
+        this.setState({ laps: this.state.laps.concat([this.state.counter])})
     }
 
-    pauseStopWatch = () =>{
+    pauseStopWatch = () => {
         this.setState({
             start: false,
         });
         clearInterval(this.interval);
     }
 
-    restart = () =>{
+    restart = () => {
         this.setState({
-            counter: 0
+            counter: 0,
+            laps:[]
         })
     }
 
-    renderBtn = () =>{
+    renderBtn = () => {
         if (this.state.start) {
             return <>
-                <ClockButton onClick = {this.addLap}>Lap</ClockButton>
+                <ClockButton onClick={this.addLap}>Lap</ClockButton>
                 <ClockButton onClick={this.pauseStopWatch}>Pause</ClockButton>
             </>
         } else {
             return <>
-                <ClockButton onClick = {this.goBack}>Back</ClockButton>
-                {this.state.counter ? <ClockButton onClick = {this.restart}>Restart</ClockButton> : null}
+                <ClockButton onClick={this.goBack}>Back</ClockButton>
+                {this.state.counter ? <ClockButton onClick={this.restart}>Restart</ClockButton> : null}
                 <ClockButton onClick={this.startStopWatch}>Start</ClockButton>
             </>
         }
     }
 
-    render () {
-        const {counter} = this.state;
+    render() {
+        const { counter } = this.state;
         return <div className="clock-page">
-            <StopWatch counter = {counter}/>
-            <div style={{marginTop: 20,width: 350,justifyContent: 'space-around'}}>
+            <StopWatch counter={counter} />
+            <ul>
+                {this.state.laps.map(function(laps,index){
+                    return <li><strong>{index +1 }</strong> /{laps} </li>;
+                })}
+            </ul>
+            <div style={{ marginTop: 20, width: 350, justifyContent: 'space-around' }}>
                 {this.renderBtn()}
             </div>
         </div>
@@ -64,4 +71,4 @@ class StopWatchPage extends Component {
 
 withRouter(StopWatchPage);
 
-export {StopWatchPage}
+export { StopWatchPage }
