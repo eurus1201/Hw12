c17de69b319d6ea8e43f22eec6121b541b6c3420
eurus1201 <khodabakhshian.s@gmail.com>
2019-08-11 11:16:01 +0430

    edited timer final

--- src/_components/timer.js
+++ src/_components/timer.js
@@ -1,15 +1,23 @@
 import React from "react";
 
-function Timer({ counter }) {
-    const sec = counter % 60;
-    const min = ((counter - sec) / 60) % 60;
-    const hour = (counter - min * 60 - sec) / 3600;
+function Timer({ timerTime }) {
+    // const sec = counter % 60;
+    // const min = ((counter - sec) / 60) % 60;
+    // const hour = (counter - min * 60 - sec) / 3600;
+
+    // const { timerTime } = this.state;
+    let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
+    let sec = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
+    let min = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
+    let hour = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
     return <div style={style}>
         <span>{hour.toLocaleString('en', { minimumIntegerDigits: 2 })}</span>
         :
     <span>{min.toLocaleString('en', { minimumIntegerDigits: 2 })}</span>
         :
     <span>{sec.toLocaleString('en', { minimumIntegerDigits: 2 })}</span>
+        :
+    <span>{centiseconds.toLocaleString('en', { minimumIntegerDigits: 2 })}</span>
     </div>;
 }
 
@@ -19,4 +27,4 @@ const style = {
     fontWeight: 'bold'
 };
 
-export {Timer}
+export { Timer }
--- src/_pages/timer.page.js
+++ src/_pages/timer.page.js
@@ -4,36 +4,82 @@ import { withRouter } from "react-router-dom";
 
 class TimerPage extends Component {
 
-  state = {
-        counter: 20,
-        strat: false
+    state = {
+        timerStart: 0,
+        strat: false,
+        timerTime: 0
     }
 
-    
+
     goBack = () => {
         this.props.history.push('/');
     }
 
+    // startTimer = () => {
+    //     this.setState({ start: true })
+    //     this.interval = setInterval(() => {
+    //         this.setState({ timerTime: this.state.timerTime - 1 })
+    //         if (this.state.timerTime === 0) {
+    //             clearInterval(this.interval)
+    //         }
+    //     }, 1000)
+    // }
+
+    // startTimer = () => {
+    //     this.setState({
+    //         start: true,
+    //         timerTime: this.state.timerTime,
+    //         timerStart: Date.now() - this.state.timerTime
+    //     });
+    //     this.timer = setInterval(() => {
+    //         this.setState({
+    //             timerTime: Date.now() - this.state.timerStart
+    //         });
+    //     }, 10);
+    // };
+
     startTimer = () => {
-        this.setState({ start: true })
-        this.interval = setInterval(() => {
-            this.setState({ counter: this.state.counter - 1 })
-            if (this.state.counter===0){
-                clearInterval(this.interval)
+        this.setState({
+            start: true,
+            timerTime: this.state.timerTime,
+            timerStart: this.state.timerTime
+        });
+        this.timer = setInterval(() => {
+            const newTime = this.state.timerTime - 10;
+            if (newTime >= 0) {
+                this.setState({
+                    timerTime: newTime
+                });
+            } else {
+                clearInterval(this.timer);
+                this.setState({ start: false });
+                alert("Countdown ended");
             }
-        }, 1000)
-    }
+        }, 10);
+    };
+
     pauseTimer = () => {
         this.setState({
             start: false,
         });
-        clearInterval(this.interval);
+        // clearInterval(this.interval);
+        clearInterval(this.timer);
     }
+
+    //     clearInterval(this.timer);
+    //   this.setState({ start: false });
+    // };
 
     restart = () => {
         this.setState({
-            counter: 20,        
-        })
+            timerStart: 0,
+            timerTime: 0
+        });
+        if (this.state.start === false) {
+            this.setState({
+                timerTime: this.state.timerStart
+            });
+        }
     }
 
     renderBtn = () => {
@@ -44,18 +90,52 @@ class TimerPage extends Component {
         } else {
             return <>
                 <ClockButton onClick={this.goBack}>Back</ClockButton>
-                {this.state.counter ? <ClockButton onClick={this.restart}>Restart</ClockButton> : null}
+                {this.state.timerTime ? <ClockButton onClick={this.restart}>Restart</ClockButton> : null}
                 <ClockButton onClick={this.startTimer}>Start</ClockButton>
             </>
         }
+
     }
+
+    adjustTimer = input => {
+        const { timerTime, strat } = this.state;
+        const max = 216000000;
+        if (!strat) {
+            if (input === "incHours" && timerTime + 3600000 < max) {
+                this.setState({ timerTime: timerTime + 3600000 });
+            } else if (input === "decHours" && timerTime - 3600000 >= 0) {
+                this.setState({ timerTime: timerTime - 3600000 });
+            } else if (input === "incMinutes" && timerTime + 60000 < max) {
+                this.setState({ timerTime: timerTime + 60000 });
+            } else if (input === "decMinutes" && timerTime - 60000 >= 0) {
+                this.setState({ timerTime: timerTime - 60000 });
+            } else if (input === "incSeconds" && timerTime + 1000 < max) {
+                this.setState({ timerTime: timerTime + 1000 });
+            } else if (input === "decSeconds" && timerTime - 1000 >= 0) {
+                this.setState({ timerTime: timerTime - 1000 });
+            }
+        }
+    };
 
 
     render() {
-        const { counter } = this.state;
+        // const { timerTime } = this.state;
+        const { timerTime, timerStart, start } = this.state;
+        let seconds = ("0" + (Math.floor((timerTime / 1000) % 60) % 60)).slice(-2);
+        let minutes = ("0" + Math.floor((timerTime / 60000) % 60)).slice(-2);
+        let hours = ("0" + Math.floor((timerTime / 3600000) % 60)).slice(-2);
+
         return <div className=" clock-page">
-            <Timer counter={counter} />
-            
+            <Timer timerTime={timerTime} />
+            {/* <div className="Countdown-label">{hours} : {minutes} : {seconds}</div> */}
+            <div className="Countdown-display">
+                <button onClick={() => this.adjustTimer("incHours")}>&#8679;</button>
+                <button onClick={() => this.adjustTimer("incMinutes")}>&#8679;</button>
+                <button onClick={() => this.adjustTimer("incSeconds")}>&#8679;</button>
+                <button onClick={() => this.adjustTimer("decHours")}>&#8681;</button>
+                <button onClick={() => this.adjustTimer("decMinutes")}>&#8681;</button>
+                <button onClick={() => this.adjustTimer("decSeconds")}>&#8681;</button>
+            </div>
             <div style={{ marginTop: 20, width: 350, justifyContent: 'space-around' }}>
                 {this.renderBtn()}
             </div>
